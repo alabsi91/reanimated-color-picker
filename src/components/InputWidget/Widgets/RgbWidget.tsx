9a9037b014573d3912d6ad6c7891dfd7483c18b8
Ahmed Alabsi <alabsi91@gmail.com>
2023-03-09 17:42:33 +0300

    fix opacity decimal input

--- src/components/InputWidget/Widgets/RgbWidget.tsx
+++ src/components/InputWidget/Widgets/RgbWidget.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { View, TextInput, Text } from 'react-native';
 import { runOnJS, useDerivedValue } from 'react-native-reanimated';
 
@@ -20,6 +20,7 @@ export default function RgbWidget({
   const [rgbValues, setRgbValues] = useState(colorKit.RGB(returnedResults().rgba).object());
 
   const isFocesed = useRef(false);
+  const alphaInputRef = useRef<TextInput>(null!);
 
   const updateText = () => {
     const { r, g, b, a } = colorKit.RGB(returnedResults().rgba).object();
@@ -33,29 +34,25 @@ export default function RgbWidget({
 
   const onRedChange = (text: string) => {
     let red = +text;
-    if (typeof red !== 'number' || isNaN(red)) return;
-    red = clamp(red, 255);
+    red = clamp(isNaN(red) ? 0 : red, 255);
     setRgbValues(prev => ({ ...prev, r: red }));
     onChange(`rgba(${red}, ${rgbValues.g}, ${rgbValues.b}, ${rgbValues.a})`);
   };
   const onGreenChange = (text: string) => {
     let green = +text;
-    if (typeof green !== 'number' || isNaN(green)) return;
-    green = clamp(green, 255);
+    green = clamp(isNaN(green) ? 0 : green, 255);
     setRgbValues(prev => ({ ...prev, g: green }));
     onChange(`rgba(${rgbValues.r}, ${green}, ${rgbValues.b}, ${rgbValues.a})`);
   };
   const onBlueChange = (text: string) => {
     let blue = +text;
-    if (typeof blue !== 'number' || isNaN(blue)) return;
-    blue = clamp(blue, 255);
+    blue = clamp(isNaN(blue) ? 0 : blue, 255);
     setRgbValues(prev => ({ ...prev, b: blue }));
     onChange(`rgba(${rgbValues.r}, ${rgbValues.g}, ${blue}, ${rgbValues.a})`);
   };
   const onAlphaChange = (text: string) => {
-    let alpha = +text;
-    if (typeof alpha !== 'number' || isNaN(alpha)) return;
-    alpha = clamp(alpha, 1);
+    let alpha = parseFloat(text);
+    alpha = clamp(isNaN(alpha) ? 0 : alpha, 1);
     setRgbValues(prev => ({ ...prev, a: alpha }));
     onChange(`rgba(${rgbValues.r}, ${rgbValues.g}, ${rgbValues.b}, ${alpha})`);
   };
@@ -65,17 +62,25 @@ export default function RgbWidget({
   };
   const onBlur = () => {
     isFocesed.current = false;
+    if (!rgbValues.a) alphaInputRef.current.setNativeProps({ text: rgbValues.a + '' || '0' });
   };
+
+  useEffect(() => {
+    alphaInputRef.current.setNativeProps({ text: rgbValues.a + '' || '0' });
+  }, [rgbValues.a]);
+
   return (
     <>
       <View style={styles.inputsContainer}>
         <TextInput
           style={[styles.input, inputStyle]}
-          keyboardType='number-pad'
           value={rgbValues.r + ''}
           onChangeText={onRedChange}
           onBlur={onBlur}
           onFocus={onFocus}
+          keyboardType='number-pad'
+          autoComplete='off'
+          autoCorrect={false}
           selectTextOnFocus
         />
         <Text style={[styles.inputTitle, inputTitleStyle]}>R</Text>
@@ -83,11 +88,13 @@ export default function RgbWidget({
       <View style={styles.inputsContainer}>
         <TextInput
           style={[styles.input, inputStyle]}
-          keyboardType='number-pad'
           value={rgbValues.g + ''}
           onChangeText={onGreenChange}
           onBlur={onBlur}
           onFocus={onFocus}
+          keyboardType='number-pad'
+          autoComplete='off'
+          autoCorrect={false}
           selectTextOnFocus
         />
         <Text style={[styles.inputTitle, inputTitleStyle]}>G</Text>
@@ -95,23 +102,28 @@ export default function RgbWidget({
       <View style={styles.inputsContainer}>
         <TextInput
           style={[styles.input, inputStyle]}
-          keyboardType='number-pad'
           value={rgbValues.b + ''}
           onChangeText={onBlueChange}
           onBlur={onBlur}
           onFocus={onFocus}
+          keyboardType='number-pad'
+          autoComplete='off'
+          autoCorrect={false}
           selectTextOnFocus
         />
         <Text style={[styles.inputTitle, inputTitleStyle]}>B</Text>
       </View>
       <View style={styles.inputsContainer}>
         <TextInput
+          ref={alphaInputRef}
           style={[styles.input, inputStyle]}
-          keyboardType='number-pad'
-          value={rgbValues.a + ''}
+          defaultValue={rgbValues.a + ''}
           onChangeText={onAlphaChange}
           onBlur={onBlur}
           onFocus={onFocus}
+          keyboardType='decimal-pad'
+          autoComplete='off'
+          autoCorrect={false}
           selectTextOnFocus
         />
         <Text style={[styles.inputTitle, inputTitleStyle]}>A</Text>
