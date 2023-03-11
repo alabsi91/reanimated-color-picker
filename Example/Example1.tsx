3cabf1db09005bb3bc93e13608e3c27ed75924ff
Ahmed Alabsi <alabsi91@gmail.com>
2023-03-11 18:36:57 +0300

    Update Example

--- Example/Example1.tsx
+++ Example/Example1.tsx
@@ -1,20 +1,9 @@
 import React, { useState } from 'react';
-import { Button, Modal, StyleSheet, View } from 'react-native';
+import { Button, KeyboardAvoidingView, Modal, ScrollView, StyleSheet, View } from 'react-native';
 import { SharedValue } from 'react-native-reanimated';
-import ColorPicker, { Panel1, Swatches, Preview, OpacitySlider, HueSlider } from 'reanimated-color-picker';
+import ColorPicker, { Panel1, Swatches, Preview, OpacitySlider, HueSlider, InputWidget } from 'reanimated-color-picker';
 
-const customSwatches = [
-  '#001219',
-  '#005f73',
-  '#0a9396',
-  '#94d2bd',
-  '#e9d8a6',
-  '#ee9b00',
-  '#ca6702',
-  '#bb3e03',
-  '#ae2012',
-  '#9b2226',
-];
+const customSwatches = ['#001219', '#005f73', '#0a9396', '#94d2bd', '#e9d8a6'];
 
 interface porpsType {
   color: SharedValue<string>;
@@ -28,31 +17,40 @@ export default function Example1({ onSelectColor, color }: porpsType) {
     <>
       <Button title='Color Picker 1' onPress={() => setShowModal(true)} />
 
-      <Modal onRequestClose={() => setShowModal(false)} visible={showModal} animationType='slide' statusBarTranslucent>
-        <View style={styles.container}>
+      <Modal onRequestClose={() => setShowModal(false)} visible={showModal} animationType='slide'>
+        <ScrollView style={styles.container} contentContainerStyle={{ flex: 1 }}>
           <ColorPicker
             value={color.value}
             sliderThickness={25}
             thumbSize={30}
-            style={{ width: '75%', justifyContent: 'center' }}
+            style={{ width: '75%', justifyContent: 'space-around' }}
             onComplete={onSelectColor}
           >
-            <Panel1 style={[{ height: 200, marginBottom: 30 }, styles.shadow]} />
+            <View style={{ flex: 1, justifyContent: 'center' }}>
+              <Panel1 style={styles.shadow} />
 
-            <View style={styles.hueOpacityPreviewContainer}>
-              <Preview style={[styles.previewStyle, styles.shadow]} hideInitialColor hideText />
+              <View style={styles.hueOpacityPreviewContainer}>
+                <Preview style={[styles.previewStyle, styles.shadow]} hideInitialColor hideText />
 
-              <View style={{ flexDirection: 'column', justifyContent: 'center', flex: 1 }}>
-                <HueSlider thumbShape='triangleDown' style={[{ marginBottom: 20 }, styles.shadow]} thumbColor='#00121a' />
-                <OpacitySlider thumbShape='triangleUp' style={styles.shadow} thumbColor='#00121a' />
+                <View style={{ flexDirection: 'column', justifyContent: 'center', flex: 1 }}>
+                  <HueSlider thumbShape='triangleDown' style={[{ marginBottom: 20 }, styles.shadow]} thumbColor='#00121a' />
+                  <OpacitySlider thumbShape='triangleUp' style={styles.shadow} thumbColor='#00121a' />
+                </View>
               </View>
+
+              <KeyboardAvoidingView behavior='position' keyboardVerticalOffset={150}>
+                <InputWidget
+                  containerStyle={{ backgroundColor: '#e8e8e8', marginTop: 20 }}
+                  inputStyle={{ marginHorizontal: 5 }}
+                />
+              </KeyboardAvoidingView>
             </View>
 
-            <Swatches swatchStyle={styles.swatchStyle} colors={customSwatches} />
+            <Swatches style={styles.swatchesContainer} swatchStyle={styles.swatchStyle} colors={customSwatches} />
           </ColorPicker>
 
           <Button title='Close' onPress={() => setShowModal(false)} />
-        </View>
+        </ScrollView>
       </Modal>
     </>
   );
@@ -61,13 +59,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#e8e8e8',
-    paddingBottom: 0,
+    paddingBottom: 10,
   },
   hueOpacityPreviewContainer: {
     flexWrap: 'nowrap',
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 60,
+    marginVertical: 30,
   },
   previewStyle: {
     width: 55,
@@ -75,12 +73,20 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     marginEnd: 20,
   },
+  swatchesContainer: {
+    flex: 0.25,
+    alignItems: 'center',
+    flexWrap: 'nowrap',
+    gap: 10,
+  },
   swatchStyle: {
     borderRadius: 20,
     height: 40,
     width: 40,
-    marginHorizontal: 10,
-    marginBottom: 15,
+    margin: 0,
+    marginBottom: 0,
+    marginHorizontal: 0,
+    marginVertical: 0,
   },
   shadow: {
     shadowColor: '#000',
--- Example/Example4.tsx
+++ Example/Example4.tsx
@@ -36,16 +36,16 @@ export default function Example4({ onSelectColor, color }: porpsType) {
             />
 
             <Text style={styles.sliderLabel}>Hue:</Text>
-            <HueSlider style={styles.sliderStyle} />
+            <HueSlider style={styles.sliderStyle} adaptSpectrum />
 
             <Text style={styles.sliderLabel}>Brightness:</Text>
-            <BrightnessSlider style={styles.sliderStyle} />
+            <BrightnessSlider style={styles.sliderStyle} adaptSpectrum />
 
             <Text style={styles.sliderLabel}>Saturation:</Text>
-            <SaturationSlider style={styles.sliderStyle} />
+            <SaturationSlider style={styles.sliderStyle} adaptSpectrum />
 
             <Text style={styles.sliderLabel}>Opacity:</Text>
-            <OpacitySlider style={styles.sliderStyle} />
+            <OpacitySlider style={styles.sliderStyle} adaptSpectrum />
 
             <Swatches style={{ marginTop: 30 }} swatchStyle={[styles.swatchStyle, styles.shadow]} colors={customSwatches} />
           </ColorPicker>
