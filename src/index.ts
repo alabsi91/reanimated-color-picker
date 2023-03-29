83e46e215b5d1fce176325d1249e43b08b3f68ed
Ahmed Alabsi <alabsi91@gmail.com>
2023-03-29 14:45:56 +0300

    Implement `Panel4`

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,13 @@ export { colorKit };
 export { Panel1 } from '@panels/Panel1';
 export { Panel2 } from '@panels/Panel2';
 export { Panel3 } from '@panels/Panel3';
+export { Panel4 } from '@panels/Panel4';
 export { HueSlider } from '@sliders/HueSlider';
 export { SaturationSlider } from '@sliders/SaturationSlider';
 export { BrightnessSlider } from '@sliders/BrightnessSlider';
+export { OpacitySlider } from '@sliders/OpacitySlider';
 export { Preview } from './components/Preview';
 export { PreviewText } from './components/PreviewText';
-export { OpacitySlider } from '@sliders/OpacitySlider';
 export { Swatches } from './components/Swatches';
 export { InputWidget } from './components/InputWidget/InputWidget';
 
