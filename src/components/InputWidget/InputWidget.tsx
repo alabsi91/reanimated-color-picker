5c1fd0bd9bf3a1d997ae1971621506c48ee82d29
Ahmed Alabsi <alabsi91@gmail.com>
2023-04-04 12:47:50 +0300

    Remove SVG from `InputWidget`

--- src/components/InputWidget/InputWidget.tsx
+++ src/components/InputWidget/InputWidget.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { View, Text, Pressable, Image, Platform, StyleSheet } from 'react-native';
+import { View, Text, Pressable, Image, StyleSheet } from 'react-native';
 
 import { styles } from '@styles';
 import { getStyle } from '@utils';
@@ -81,19 +81,7 @@ export function InputWidget({
       {formats.length > 1 && (
         <View style={{ width: iconWidth }}>
           <Pressable onPress={cycle}>
-            {Platform.select({
-              web: (
-                <svg
-                  style={buttonIconStyle as React.CSSProperties}
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='0 0 24 24'
-                  fill={iconColor}
-                >
-                  <path d='M12 5.83L15.17 9l1.41-1.41L12 3 7.41 7.59 8.83 9 12 5.83zm0 12.34L8.83 15l-1.41 1.41L12 21l4.59-4.59L15.17 15 12 18.17z' />
-                </svg>
-              ),
-              default: <Image style={[buttonIconStyle, { tintColor: iconColor }]} source={require('@assets/arrow-icon.png')} />,
-            })}
+            <Image style={[buttonIconStyle, { tintColor: iconColor }]} source={require('@assets/arrow-icon.png')} />
           </Pressable>
           <Text style={styles.inputTitle}> </Text>
         </View>
