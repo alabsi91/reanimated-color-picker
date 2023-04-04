eb12b4395f7c9ed1e2c97b45a10cc7105e3d1771
Ahmed Alabsi <alabsi91@gmail.com>
2023-04-04 12:49:06 +0300

    Implement RGB sliders

---
+++ src/components/Sliders/RGB/GreenSlider.tsx
@@ -0,0 +1,167 @@
+import React, { useContext } from 'react';
+import { GestureDetector, Gesture } from 'react-native-gesture-handler';
+import Animated, { runOnJS, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+
+import { clamp, getStyle, HSVA2RGBA, isRtl, isWeb, RGB2HSVA } from '@utils';
+import CTX from '@context';
+import Thumb from '@thumb';
+
+import type { RgbSliderProps } from '@types';
+import type { LayoutChangeEvent } from 'react-native';
+import type { PanGestureHandlerEventPayload } from 'react-native-gesture-handler';
+
+export function GreenSlider({
+  thumbShape: localThumbShape,
+  thumbSize: localThumbSize,
+  thumbColor: localThumbColor,
+  boundedThumb: localBoundedThumb,
+  renderThumb: localRenderThumb,
+  thumbStyle: localThumbStyle,
+  thumbInnerStyle: localThumbInnerStyle,
+  sliderThickness: localSliderThickness,
+  style = {},
+  vertical = false,
+  reverse = false,
+}: RgbSliderProps) {
+  const {
+    hueValue,
+    saturationValue,
+    brightnessValue,
+    onGestureChange,
+    onGestureEnd,
+    thumbSize: globalThumbSize,
+    thumbShape: globalThumbShape,
+    thumbColor: globalThumbColor,
+    boundedThumb: globalBoundedThumb,
+    renderThumb: globalRenderThumb,
+    thumbStyle: globalThumbStyle,
+    thumbInnerStyle: globalThumbInnerStyle,
+    sliderThickness: globalSliderThickness,
+  } = useContext(CTX);
+
+  const thumbShape = localThumbShape ?? globalThumbShape,
+    thumbSize = localThumbSize ?? globalThumbSize,
+    thumbColor = localThumbColor ?? globalThumbColor,
+    boundedThumb = localBoundedThumb ?? globalBoundedThumb,
+    renderThumb = localRenderThumb ?? globalRenderThumb,
+    thumbStyle = localThumbStyle ?? globalThumbStyle ?? {},
+    thumbInnerStyle = localThumbInnerStyle ?? globalThumbInnerStyle ?? {},
+    sliderThickness = localSliderThickness ?? globalSliderThickness;
+
+  const borderRadius = getStyle(style, 'borderRadius') ?? 5,
+    getWidth = getStyle(style, 'width'),
+    getHeight = getStyle(style, 'height');
+
+  const width = useSharedValue(vertical ? sliderThickness : typeof getWidth === 'number' ? getWidth : 0),
+    height = useSharedValue(!vertical ? sliderThickness : typeof getHeight === 'number' ? getHeight : 0);
+
+  const handleScale = useSharedValue(1);
+
+  const handleStyle = useAnimatedStyle(() => {
+    const { g } = HSVA2RGBA(hueValue.value, saturationValue.value, brightnessValue.value);
+
+    const length = (vertical ? height.value : width.value) - (boundedThumb ? thumbSize : 0),
+      percent = (g / 255) * length,
+      pos = (reverse ? length - percent : percent) - (boundedThumb ? 0 : thumbSize / 2),
+      posY = vertical ? pos : height.value / 2 - thumbSize / 2,
+      posX = vertical ? width.value / 2 - thumbSize / 2 : pos;
+    return {
+      transform: [{ translateY: posY }, { translateX: posX }, { scale: handleScale.value }],
+    };
+  }, [localThumbSize, vertical, reverse]);
+
+  const onGestureUpdate = ({ x, y }: PanGestureHandlerEventPayload) => {
+    'worklet';
+
+    const { r, g, b } = HSVA2RGBA(hueValue.value, saturationValue.value, brightnessValue.value);
+
+    const length = (vertical ? height.value : width.value) - (boundedThumb ? thumbSize : 0),
+      pos = clamp((vertical ? y : x) - (boundedThumb ? thumbSize / 2 : 0), length),
+      value = Math.round((pos / length) * 255),
+      newGreenValue = reverse ? 255 - value : value;
+
+    if (newGreenValue === g) return;
+
+    const { h, s, v } = RGB2HSVA(r, newGreenValue, b);
+
+    hueValue.value = h;
+    saturationValue.value = s;
+    brightnessValue.value = v;
+
+    runOnJS(onGestureChange)();
+  };
+  const onGestureBegin = (event: PanGestureHandlerEventPayload) => {
+    'worklet';
+    handleScale.value = withTiming(1.2, { duration: 100 });
+    onGestureUpdate(event);
+  };
+  const onGestureFinish = () => {
+    'worklet';
+    handleScale.value = withTiming(1, { duration: 100 });
+    runOnJS(onGestureEnd)();
+  };
+
+  const pan = Gesture.Pan().onBegin(onGestureBegin).onUpdate(onGestureUpdate).onEnd(onGestureFinish);
+  const tap = Gesture.Tap().onTouchesUp(onGestureFinish);
+  const longPress = Gesture.LongPress().onTouchesUp(onGestureFinish);
+  const composed = Gesture.Exclusive(pan, tap, longPress);
+
+  const onLayout = ({ nativeEvent: { layout } }: LayoutChangeEvent) => {
+    if (!vertical) width.value = withTiming(layout.width, { duration: 5 });
+    if (vertical) height.value = withTiming(layout.height, { duration: 5 });
+  };
+
+  const redBlue = useAnimatedStyle(() => {
+    const { r, b } = HSVA2RGBA(hueValue.value, saturationValue.value, brightnessValue.value);
+    if (isWeb) {
+      const deg = vertical ? (reverse ? 180 : 0) : reverse ? 90 : 270;
+      return { background: `linear-gradient(${deg}deg, rgb(${r}, 255, ${b}) 0%, rgb(${r}, 0, ${b}) 100%)` };
+    }
+    return { backgroundColor: `rgb(${r}, 0, ${b})` };
+  });
+
+  const imageStyle = useAnimatedStyle(() => {
+    if (isWeb) return {};
+
+    const imageRotate = vertical ? (reverse ? '90deg' : '270deg') : reverse ? '0deg' : '180deg';
+    const imageTranslateY = ((height.value - width.value) / 2) * ((reverse && isRtl) || (!reverse && !isRtl) ? -1 : 1);
+    const { r, b } = HSVA2RGBA(hueValue.value, saturationValue.value, brightnessValue.value);
+
+    return {
+      tintColor: `rgb(${r}, 255, ${b})`,
+      width: vertical ? height.value : '100%',
+      height: vertical ? width.value : '100%',
+      borderRadius,
+      transform: [
+        { rotate: imageRotate },
+        { translateX: vertical ? ((height.value - width.value) / 2) * (reverse ? 1 : -1) : 0 },
+        { translateY: vertical ? imageTranslateY : 0 },
+      ],
+    };
+  }, [vertical, reverse, sliderThickness]);
+
+  const thicknessStyle = vertical ? { width: sliderThickness } : { height: sliderThickness };
+
+  return (
+    <GestureDetector gesture={composed}>
+      <Animated.View
+        onLayout={onLayout}
+        style={[{ borderRadius }, style, { position: 'relative', borderWidth: 0, padding: 0 }, thicknessStyle, redBlue]}
+      >
+        {!isWeb && <Animated.Image source={require('@assets/Brightness.png')} style={imageStyle} />}
+        <Thumb
+          {...{
+            thumbShape,
+            thumbSize,
+            thumbColor,
+            renderThumb,
+            handleStyle,
+            innerStyle: thumbInnerStyle,
+            style: thumbStyle,
+            vertical,
+          }}
+        />
+      </Animated.View>
+    </GestureDetector>
+  );
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { StyleSheet, I18nManager } from 'react-native';
+import { StyleSheet, I18nManager, Platform } from 'react-native';
 
 import type { StyleProp, ViewStyle } from 'react-native';
 
@@ -12,7 +12,8 @@ export function clamp(v: number, max: number) {
   return Math.min(Math.max(v, 0), max);
 }
 
-export function hsva2Hsla(h: number, s: number, v: number, a = 1) {
+/** - Convert `HSV` color to an `HSLA` object representation */
+export function HSVA2HSLA(h: number, s: number, v: number, a = 1) {
   'worklet';
 
   s = s / 100;
@@ -25,4 +26,100 @@ export function hsva2Hsla(h: number, s: number, v: number, a = 1) {
   return `hsla(${h}, ${sln * 100}%, ${l * 100}%, ${a})`;
 }
 
+/** - Convert `HSV` color to an `RGBA` object representation */
+export function HSVA2RGBA(h: number, s: number, v: number, a = 1) {
+  'worklet';
+
+  h = h / 360;
+  s = s / 100;
+  v = v / 100;
+
+  const i = Math.floor(h * 6),
+    f = h * 6 - i,
+    p = v * (1 - s),
+    q = v * (1 - f * s),
+    t = v * (1 - (1 - f) * s);
+
+  let r = 0,
+    g = 0,
+    b = 0;
+  switch (i % 6) {
+    case 0:
+      r = v;
+      g = t;
+      b = p;
+      break;
+    case 1:
+      r = q;
+      g = v;
+      b = p;
+      break;
+    case 2:
+      r = p;
+      g = v;
+      b = t;
+      break;
+    case 3:
+      r = p;
+      g = q;
+      b = v;
+      break;
+    case 4:
+      r = t;
+      g = p;
+      b = v;
+      break;
+    case 5:
+      r = v;
+      g = p;
+      b = q;
+      break;
+  }
+
+  return {
+    r: clamp(r * 255, 255),
+    g: clamp(g * 255, 255),
+    b: clamp(b * 255, 255),
+    a: clamp(a, 1),
+  };
+}
+
+/** - Convert `RGB` or `RGBA` color to an `HSVA`  object representation */
+export function RGB2HSVA(r: number, g: number, b: number, a = 1) {
+  'worklet';
+
+  r = r / 255;
+  g = g / 255;
+  b = b / 255;
+
+  const max = Math.max(r, g, b),
+    min = Math.min(r, g, b),
+    d = max - min;
+
+  let v = max,
+    h = 0,
+    s = max === 0 ? 0 : d / max;
+
+  if (max === min) {
+    h = 0;
+  } else {
+    if (max === r) {
+      h = (g - b) / d + (g < b ? 6 : 0);
+    } else if (max === g) {
+      h = (b - r) / d + 2;
+    } else if (max === b) {
+      h = (r - g) / d + 4;
+    }
+    h = h / 6;
+  }
+
+  return {
+    h: clamp(h * 360, 360),
+    s: clamp(s * 100, 100),
+    v: clamp(v * 100, 100),
+    a: clamp(a, 1),
+  };
+}
+
 export const isRtl = I18nManager.isRTL;
+export const isWeb = Platform.OS === 'web';
