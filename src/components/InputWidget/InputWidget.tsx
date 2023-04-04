d6ae6d634545ba9c66081931a279df9c3dce7908
Ahmed Alabsi <alabsi91@gmail.com>
2023-04-04 12:25:01 +0300

    Fix `InputWidget` icon color for IOS

--- src/components/InputWidget/InputWidget.tsx
+++ src/components/InputWidget/InputWidget.tsx
@@ -92,14 +92,7 @@ export function InputWidget({
                   <path d='M12 5.83L15.17 9l1.41-1.41L12 3 7.41 7.59 8.83 9 12 5.83zm0 12.34L8.83 15l-1.41 1.41L12 21l4.59-4.59L15.17 15 12 18.17z' />
                 </svg>
               ),
-              default: (
-                <Image
-                  // @ts-expect-error
-                  tintColor={iconColor}
-                  style={buttonIconStyle}
-                  source={require('@assets/arrow-icon.png')}
-                />
-              ),
+              default: <Image style={[buttonIconStyle, { tintColor: iconColor }]} source={require('@assets/arrow-icon.png')} />,
             })}
           </Pressable>
           <Text style={styles.inputTitle}> </Text>
