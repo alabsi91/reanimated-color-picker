8c5178cc22ed2c4672e4adb2f51a45cd2cdeb29d
alabsi91 <alabsi91.gmail.com>
2023-10-03 19:51:16 +0300

    Build docs website

--- docs/assets/js/runtime~main.f3dc5797.js
+++ docs/assets/js/runtime~main.f3dc5797.js
@@ -5,25 +5,25 @@
     r,
     a,
     f,
-    c = {},
-    o = {};
+    o = {},
+    c = {};
   function n(e) {
-    var t = o[e];
+    var t = c[e];
     if (void 0 !== t) return t.exports;
-    var r = (o[e] = { id: e, loaded: !1, exports: {} });
-    return c[e].call(r.exports, r, r.exports, n), (r.loaded = !0), r.exports;
+    var r = (c[e] = { id: e, loaded: !1, exports: {} });
+    return o[e].call(r.exports, r, r.exports, n), (r.loaded = !0), r.exports;
   }
-  (n.m = c),
-    (n.c = o),
+  (n.m = o),
+    (n.c = c),
     (e = []),
     (n.O = (t, r, a, f) => {
       if (!r) {
-        var c = 1 / 0;
+        var o = 1 / 0;
         for (i = 0; i < e.length; i++) {
           (r = e[i][0]), (a = e[i][1]), (f = e[i][2]);
-          for (var o = !0, d = 0; d < r.length; d++)
-            (!1 & f || c >= f) && Object.keys(n.O).every(e => n.O[e](r[d])) ? r.splice(d--, 1) : ((o = !1), f < c && (c = f));
-          if (o) {
+          for (var c = !0, d = 0; d < r.length; d++)
+            (!1 & f || o >= f) && Object.keys(n.O).every(e => n.O[e](r[d])) ? r.splice(d--, 1) : ((c = !1), f < o && (o = f));
+          if (c) {
             e.splice(i--, 1);
             var b = a();
             void 0 !== b && (t = b);
@@ -48,11 +48,11 @@
       }
       var f = Object.create(null);
       n.r(f);
-      var c = {};
+      var o = {};
       t = t || [null, r({}), r([]), r(r)];
-      for (var o = 2 & a && e; 'object' == typeof o && !~t.indexOf(o); o = r(o))
-        Object.getOwnPropertyNames(o).forEach(t => (c[t] = () => e[t]));
-      return (c.default = () => e), n.d(f, c), f;
+      for (var c = 2 & a && e; 'object' == typeof c && !~t.indexOf(c); c = r(c))
+        Object.getOwnPropertyNames(c).forEach(t => (o[t] = () => e[t]));
+      return (o.default = () => e), n.d(f, o), f;
     }),
     (n.d = (e, t) => {
       for (var r in t) n.o(t, r) && !n.o(e, r) && Object.defineProperty(e, r, { enumerable: !0, get: t[r] });
@@ -121,7 +121,7 @@
         753: '498fd52e',
         756: '050574b1',
         817: '254efa08',
-        835: 'e84f2c3e',
+        835: '8d3b0367',
         841: '7bcc0e06',
         873: 'ea03b495',
         918: '80c081ca',
@@ -143,33 +143,33 @@
     (n.o = (e, t) => Object.prototype.hasOwnProperty.call(e, t)),
     (a = {}),
     (f = 'my-docs:'),
-    (n.l = (e, t, r, c) => {
+    (n.l = (e, t, r, o) => {
       if (a[e]) a[e].push(t);
       else {
-        var o, d;
+        var c, d;
         if (void 0 !== r)
           for (var b = document.getElementsByTagName('script'), i = 0; i < b.length; i++) {
             var u = b[i];
             if (u.getAttribute('src') == e || u.getAttribute('data-webpack') == f + r) {
-              o = u;
+              c = u;
               break;
             }
           }
-        o ||
+        c ||
           ((d = !0),
-          ((o = document.createElement('script')).charset = 'utf-8'),
-          (o.timeout = 120),
-          n.nc && o.setAttribute('nonce', n.nc),
-          o.setAttribute('data-webpack', f + r),
-          (o.src = e)),
+          ((c = document.createElement('script')).charset = 'utf-8'),
+          (c.timeout = 120),
+          n.nc && c.setAttribute('nonce', n.nc),
+          c.setAttribute('data-webpack', f + r),
+          (c.src = e)),
           (a[e] = [t]);
         var l = (t, r) => {
-            (o.onerror = o.onload = null), clearTimeout(s);
+            (c.onerror = c.onload = null), clearTimeout(s);
             var f = a[e];
-            if ((delete a[e], o.parentNode && o.parentNode.removeChild(o), f && f.forEach(e => e(r)), t)) return t(r);
+            if ((delete a[e], c.parentNode && c.parentNode.removeChild(c), f && f.forEach(e => e(r)), t)) return t(r);
           },
-          s = setTimeout(l.bind(null, void 0, { type: 'timeout', target: o }), 12e4);
-        (o.onerror = l.bind(null, o.onerror)), (o.onload = l.bind(null, o.onload)), d && document.head.appendChild(o);
+          s = setTimeout(l.bind(null, void 0, { type: 'timeout', target: c }), 12e4);
+        (c.onerror = l.bind(null, c.onerror)), (c.onload = l.bind(null, c.onload)), d && document.head.appendChild(c);
       }
     }),
     (n.r = e => {
@@ -226,19 +226,19 @@
           else {
             var f = new Promise((r, f) => (a = e[t] = [r, f]));
             r.push((a[2] = f));
-            var c = n.p + n.u(t),
-              o = new Error();
+            var o = n.p + n.u(t),
+              c = new Error();
             n.l(
-              c,
+              o,
               r => {
                 if (n.o(e, t) && (0 !== (a = e[t]) && (e[t] = void 0), a)) {
                   var f = r && ('load' === r.type ? 'missing' : r.type),
-                    c = r && r.target && r.target.src;
-                  (o.message = 'Loading chunk ' + t + ' failed.\n(' + f + ': ' + c + ')'),
-                    (o.name = 'ChunkLoadError'),
-                    (o.type = f),
-                    (o.request = c),
-                    a[1](o);
+                    o = r && r.target && r.target.src;
+                  (c.message = 'Loading chunk ' + t + ' failed.\n(' + f + ': ' + o + ')'),
+                    (c.name = 'ChunkLoadError'),
+                    (c.type = f),
+                    (c.request = o),
+                    a[1](c);
                 }
               },
               'chunk-' + t,
@@ -250,15 +250,15 @@
       var t = (t, r) => {
           var a,
             f,
-            c = r[0],
-            o = r[1],
+            o = r[0],
+            c = r[1],
             d = r[2],
             b = 0;
-          if (c.some(t => 0 !== e[t])) {
-            for (a in o) n.o(o, a) && (n.m[a] = o[a]);
+          if (o.some(t => 0 !== e[t])) {
+            for (a in c) n.o(c, a) && (n.m[a] = c[a]);
             if (d) var i = d(n);
           }
-          for (t && t(r); b < c.length; b++) (f = c[b]), n.o(e, f) && e[f] && e[f][0](), (e[f] = 0);
+          for (t && t(r); b < o.length; b++) (f = o[b]), n.o(e, f) && e[f] && e[f][0](), (e[f] = 0);
           return n.O(i);
         },
         r = (self.webpackChunkmy_docs = self.webpackChunkmy_docs || []);
