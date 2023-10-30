1a0ddc12d9ff601d7063d0433cbcd7cc726cbe15
Ahmed ALAbsi <alabsi91@gmail.com>
2023-10-30 18:57:41 +0300

    setup jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest';
+
+const config: Config = {
+  testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
+};
+
+export default config;
