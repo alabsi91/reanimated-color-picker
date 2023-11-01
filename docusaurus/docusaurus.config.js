a085ec4e38b346aac9053b8f1e3ac611d007ad53
Ahmed ALAbsi <alabsi91@gmail.com>
2023-11-01 13:52:33 +0300

    Upgrade docusaurus to v3.0.0

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -1,8 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const { themes } = require('prism-react-renderer');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -80,8 +79,8 @@ const config = {
         copyright: 'Released under the MIT License.',
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: themes.github,
+        darkTheme: themes.dracula,
       },
     }),
 };
