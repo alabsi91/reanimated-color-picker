4fbd73819fb7cd4dc0a086199e822dca644a4ace
Ahmed ALAbsi <alabsi91@gmail.com>
2023-11-02 12:22:32 +0300

    Upgrade Expo example

--- ExampleExpo/babel.config.js
+++ ExampleExpo/babel.config.js
@@ -1,7 +1,23 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        alias: {
+          'reanimated-color-picker': '../src/index',
+          '@sliders': '../src/components/Sliders/',
+          '@panels': '../src/components/Panels/',
+          '@assets': '../src/assets/',
+          '@thumb': '../src/components/Thumb/Thumb',
+          '@colorKit': '../src/colorKit/index',
+          '@context': '../src/AppContext',
+          '@styles': '../src/styles',
+          '@utils': '../src/utils',
+          '@types': '../src/types',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
