2a516ab8b66b02850c268972201fd6b5ee7e0a67
Ahmed ALAbsi <alabsi91@gmail.com>
2023-11-15 19:32:30 +0300

    Move docs

--- docusaurus/build/assets/js/d79f08f9.fed7a372.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-(self.webpackChunkmy_docs = self.webpackChunkmy_docs || []).push([
-  [346],
-  {
-    5745: s => {
-      s.exports = JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}');
-    },
-  },
-]);
--- docusaurus/scripts/copyBuild.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-fs.rm('../docs', { recursive: true }, error => {
-  if (error) console.log('error :', error);
-
-  fs.cp('./build', '../docs', { recursive: true }, error => {
-    if (error) console.log('error :', error);
-  });
-});
