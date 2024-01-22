44b559f15aea06678d44d6b1c84554e8c7260bbf
Ahmed ALAbsi <alabsi91@gmail.com>
2024-01-22 13:45:51 +0300

    Add `Panel3 hsl saturation` example

--- Example/src/App.tsx
+++ Example/src/App.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { ScrollView, StatusBar, StyleSheet, View } from 'react-native';
 import CircularHue from './CircularHue';
 import HorizontalHsbSliders from './HsbHorizontal';
+import VerticalHsbSliders from './HsbVertical';
 import HorizontalHslSliders from './HslHorizontal';
-import HorizontalRgbSliders from './RgbHorizontal';
+import VerticalHslSliders from './HslVertical';
 import Panel1Example from './Panel1';
 import Panel2Brightness from './Panel2Brightness';
 import Panel2Saturation from './Panel2Saturation';
 import WheelBrightness from './Panel3Brightness';
+import WheelHSLSaturation from './Panel3HslSaturation';
 import WheelSaturation from './Panel3Saturation';
 import Panel4Example from './Panel4';
 import Panel5Example from './Panel5';
-import VerticalHsbSliders from './HsbVertical';
-import VerticalHslSliders from './HslVertical';
+import HorizontalRgbSliders from './RgbHorizontal';
 import VerticalRgbSliders from './RgbVertical';
 
 export default function App() {
@@ -28,6 +29,7 @@ export default function App() {
           <Panel2Brightness />
 
           <WheelSaturation />
+          <WheelHSLSaturation />
           <WheelBrightness />
 
           <Panel4Example />
--- ExampleExpo/App.tsx
+++ ExampleExpo/App.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { ScrollView, StatusBar, StyleSheet, View } from 'react-native';
 import CircularHue from './src/CircularHue';
 import HorizontalHsbSliders from './src/HsbHorizontal';
+import VerticalHsbSliders from './src/HsbVertical';
 import HorizontalHslSliders from './src/HslHorizontal';
-import HorizontalRgbSliders from './src/RgbHorizontal';
+import VerticalHslSliders from './src/HslVertical';
 import Panel1Example from './src/Panel1';
 import Panel2Brightness from './src/Panel2Brightness';
 import Panel2Saturation from './src/Panel2Saturation';
 import WheelBrightness from './src/Panel3Brightness';
+import WheelHSLSaturation from './src/Panel3HslSaturation';
 import WheelSaturation from './src/Panel3Saturation';
 import Panel4Example from './src/Panel4';
 import Panel5Example from './src/Panel5';
-import VerticalHsbSliders from './src/HsbVertical';
-import VerticalHslSliders from './src/HslVertical';
+import HorizontalRgbSliders from './src/RgbHorizontal';
 import VerticalRgbSliders from './src/RgbVertical';
 
 export default function App() {
@@ -28,6 +29,7 @@ export default function App() {
           <Panel2Brightness />
 
           <WheelSaturation />
+          <WheelHSLSaturation />
           <WheelBrightness />
 
           <Panel4Example />
