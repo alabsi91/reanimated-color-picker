a819675607c8a3c0e6786124a4b7791b0de1105a
alabsi91 <alabsi91@gmail.com>
2025-09-11 13:09:22 +0300

    chore: update ExampleLatest dependencies to latest versions

--- ExampleLatest/metro.config.js
+++ ExampleLatest/metro.config.js
@@ -1,9 +1,27 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const path = require('path');
 const escape = require('escape-string-regexp');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 const root = path.resolve(__dirname, '..');
+
+const list = [/\/__tests__\/.*/];
+
+function escapeRegExp(pattern) {
+  if (Object.prototype.toString.call(pattern) === '[object RegExp]') {
+    return pattern.source.replace(/\/|\\\//g, '\\' + path.sep);
+  }
+
+  if (typeof pattern === 'string') {
+    const escaped = pattern.replace(/[\-\[\]\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+    return escaped.replaceAll('/', '\\' + path.sep);
+  }
+
+  throw new Error('Unexpected exclusion pattern: ' + pattern);
+}
+
+function exclusionList(additionalExclusions) {
+  return new RegExp('(' + (additionalExclusions || []).concat(list).map(escapeRegExp).join('|') + ')$');
+}
 
 function createRegExp(pkg) {
   return new RegExp(`^${escape(path.join(root, 'node_modules', pkg))}\\/.*$`);
